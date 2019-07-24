512533586a24dea130008df811ea2cbf24c24dc6
Bradley Hopper <bhopper@babbage.cs.pdx.edu>
2019-07-24 09:55:36 -0700

    basic file copying in

--- got.c
+++ got.c
@@ -4,7 +4,17 @@
 #include <linux/limits.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <errno.h>
 
+
+int copy_file(FILE * src_file, FILE * dest_file)
+{
+  int c;
+  while ((c = fgetc(src_file)) != EOF)
+  {
+    fputc(c, dest_file);
+  }
+}
 
 int main(int argc, char* argv[])
 {
@@ -23,19 +33,34 @@ int main(int argc, char* argv[])
   char gotdir[PATH_MAX];
 
   strncpy(gotdir, cwd, PATH_MAX);
-  strncat(gotdir, "/", 1);
-  strncat(gotdir, ".gotdir", 8);
+  strncat(gotdir, "/.gotdir", 9);
   if (strcmp(argv[1], "init") == 0)
   { // got init
-//    fd = fopen(
     mode_t MODE = S_IRWXU | S_IRWXG | S_IRWXO;  // Full permissions for everybody
     int d = mkdir(gotdir, MODE);                // May need to change later?
-    
+    if (d == -1)
+    {
+      switch(errno)
+      {
+        case EEXIST: write(2, "Directory already exists\n", 25);
+        default: break;
+      }
+    }
   }
 
-  write(1, cwd, strlen(cwd));
-  write(1, "\n", 1);
-  write(1, gotdir, strlen(gotdir));
-  write(1, "\n", 1);
+  if (strcmp(argv[1], "test") == 0)
+  {
+    char destp[PATH_MAX];
+    strncpy(destp, gotdir, PATH_MAX);
+    strncat(destp, "/dest", 4);
+    char srcp[PATH_MAX];
+    strncpy(srcp, cwd, PATH_MAX);
+    strncat(srcp, "/", 1);
+    strncat(srcp, argv[2], strlen(argv[2]));
+    FILE* src = fopen(srcp, "r");
+    FILE* dest = fopen(destp, "w+");
+    copy_file(src, dest);
 
+
+  }
 }
